b905daa4e0f705d8b3aeed834bc22813bb52996a
Alexandre LACOUR <100684188+Akalabete@users.noreply.github.com>
2023-12-01 16:06:14 +0100

    homepage dvp

--- themes/Lesnotes/home.php
+++ themes/Lesnotes/home.php
@@ -4,7 +4,15 @@
 <h2> 
     <?php _e('La musique au fil de vos événements.', 'lesnotes'); ?>
 </h2>
-<div class="thinsep"></div>
+<div class="thinsep">
+    <div class="lilsquare"></div>
+    
+    <div class="medsquare"></div>
+    <div class="bigsquare"></div>
+    <div class="medsquare"></div>
+    <div class="lilsquare"></div>
+
+</div>
 <div class="presentation-container">
     <div class="youtubecontainer">
     <iframe width="250" height="180" src="https://www.youtube.com/embed/videoseries?si=jvX0SY3SoTKLP_Gm&amp;list=PLxwhPt3Ve_-43A20j9CugVRuJ8hJG5vSr" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
@@ -15,26 +23,113 @@
         </h1>
     </div>
 </div>
-<div class="heavysep"></div>
+<div class="thinsep">
+    <div class="lilsquare"></div>
+    <div class="linesm"></div>
+    <div class="medsquare"></div>
+    <div class="linesm"></div>
+    <div class="bigsquare"></div>
+    <div class="linesm"></div>
+    <div class="medsquare"></div>
+    <div class="linesm"></div>
+    <div class="lilsquare"></div>
+
+</div>
 </section>
 <section id="presentation">
     <h2> 
     <?php _e('La musique au fil de vos événements est à votre tempo, latino-cubain.', 'lesnotes'); ?>
 </h2>
-<div class="thinsep"></div>
+<div class="thinsep">
+    <div class="lilsquare"></div>
+    <div class="medsquare"></div>
+    <div class="bigsquare"></div>
+    <div class="medsquare"></div>
+    <div class="lilsquare"></div>
+</div>
+<div class="groupcontainer">
+<div class="group">
+    <div class="groupphoto"></div>
+    <div class="grouppres">
+    <p>
+        <?php
+        $texte = __('Alma del sur est un trio de musique sud-américaine qui est composé de congas, flûte traversière et guitare baroque, le tout lié par le chant et la poésie à travers des histoires légendaires.', 'lesnotes');
+        $texte = str_replace('Alma del sur', '<strong>Alma del sur</strong>', $texte);
+        echo $texte;
+        ?>
+    </p>  
+    </div>
+</div>
+<div class="group">
+    <div class="groupphoto"></div>
+    <div class="grouppres">
+    <p>
+        <?php
+        $texte = __('La Isabelica est un trio de “son cubano“ composé de bongos, guitare et de l&apos;indispensable guitare tres(guitare cubaine) qui signe par sa sonorité le son cubain dans un répertoire de musique traditionnelle et populaire sélectionné pour vous. Vous retrouverez de grands classiques pour ambiancer vos événements.', 'lesnotes');
+        $texte = str_replace('La Isabelica', '<strong>La Isabelica</strong>', $texte);
+        echo $texte;
+        ?>
+    </p>
+    </div>
+</div>
+<div class="group">
+    <div class="groupphoto"></div>
+    <div class="grouppres">
+    <p>
+        <?php
+        $texte = __('La Molienda est une proposition muscale des deux formations réunies autour de la musique cubaine composée de bongos, congas, guitare, tres et flûte traversière, cet ensemble se veux plus pertinent en rythmique et en ambiance.', 'lesnotes');
+        $texte = str_replace('La Molienda', '<strong>La Molienda</strong>', $texte);
+        echo $texte;
+        ?>
+    </p>
+    </div>
+</div>
+</div>
+<div class="thinsep">
+    <div class="lilsquare"></div>
+    <div class="linesm"></div>
+    <div class="medsquare"></div>
+    <div class="linesm"></div>
+    <div class="bigsquare"></div>
+    <div class="linesm"></div>
+    <div class="medsquare"></div>
+    <div class="linesm"></div>
+    <div class="lilsquare"></div>
 
-
-<div class="heavysep"></div>
+</div>
 </section>
 
 
 <section id="prestations">
 <h2> 
-    <?php _e('Nos prestations musicales.', 'lesnotes'); ?>
+    <?php _e('Livre d&apos;or de nos prestations musicales.', 'lesnotes'); ?>
 </h2>
-<div class="thinsep"></div>
+<div class="thinsep">
+    <div class="lilsquare"></div>
+    <div class="medsquare"></div> 
+    <div class="bigsquare"></div>
+    <div class="medsquare"></div>
+    <div class="lilsquare"></div>
 
-<div class="heavysep"></div>
+</div>
+<div class="slider">
+    <div class="photo"></div>
+    <div class="photo"></div>
+    <div class="photo"></div>
+    <div class="photo"></div>
+</div>
+<div class="thinsep">
+    <div class="lilsquare"></div>
+    <div class="linesm"></div>
+    <div class="medsquare"></div>
+    <div class="linesm"></div>
+    <div class="bigsquare"></div>
+    <div class="linesm"></div>
+    <div class="medsquare"></div>
+    <div class="linesm"></div>
+    <div class="lilsquare"></div>
+
+</div>
 
 </section>
 
@@ -42,4 +137,25 @@
 <h2> 
     <?php _e('Contactez-moi pour une prestation personnalisées.', 'lesnotes'); ?>
 </h2>
+<div class="thinsep">
+    <div class="lilsquare"></div>
+    <div class="medsquare"></div>
+    <div class="bigsquare"></div>
+    <div class="medsquare"></div>
+    <div class="lilsquare"></div>
+</div>
+
+<div class="thinsep">
+    <div class="lilsquare"></div>
+    <div class="linesm"></div>
+    <div class="medsquare"></div>
+    <div class="linesm"></div>
+    <div class="bigsquare"></div>
+    <div class="linesm"></div>
+    <div class="medsquare"></div>
+    <div class="linesm"></div>
+    <div class="lilsquare"></div>
+
+</div>
+
 </section>
